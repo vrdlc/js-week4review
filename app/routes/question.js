aab2e7447081a31ca6cae5564faeabc29f899d33
vrdlc <virgilioramon.delacruz@gmail.com>
2016-04-08 08:06:25 -0700

    made small tweaks to nav-bar and answers

--- app/routes/question.js
+++ app/routes/question.js
@@ -4,16 +4,20 @@ export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('question', params.question_id);
   },
-  actions: {
-  submitAnswer(params) {
-    var newAnswer = this.store.createRecord('answer', params);
-    var question = params.question;
-    question.get('answers').addObject(newAnswer);
-    newAnswer.save().then(function() {
-      return question.save();
-    });
-    this.transitionTo('question', params.question.id);
+
+  afterModel: function(model) {
+    return model.get('answers');
   },
+  actions: {
+    submitAnswer(params) {
+      var newAnswer = this.store.createRecord('answer', params);
+      var question = params.question;
+      question.get('answers').addObject(newAnswer);
+      newAnswer.save().then(function() {
+        return question.save();
+      });
+      this.transitionTo('question', params.question.id);
+    },
     update(question, params) {
       Object.keys(params).forEach(function(key) {
         if(params[key] !==undefined && params[key] !=="") {
